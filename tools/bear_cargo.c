750a5f99dc70452263a82fbe2f0f686bdee5a140
Michael Blakey <m.k.blakey@icloud.com>
2026-01-14 18:21:42 +0000

    tools structure
    need a bear like tool to get to build commands from rust in order for this to work

---
+++ tools/bear_cargo.c
@@ -0,0 +1,22 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "cJSON.h"
+
+static void display_usage() {
+  fprintf(stderr, "usage:\n"
+                  "  bear-rs -- <cmd>\n"
+      ); 
+  exit(1);
+}
+
+
+int main(int argc, char *argv[])
+{
+  
+
+  return 0; 
+}
